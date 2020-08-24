da11f5a32ab6cefb96e5481bf5b383856e0962eb
Brian Wandell <wandell@stanford.edu>
2020-08-23 17:13:06 -0700

    Updated notes on the pyramid of visibility

--- data/datafiles/human/pyramid/s_watsonPyramid.m
+++ data/datafiles/human/pyramid/s_watsonPyramid.m
@@ -1,28 +1,144 @@
 %% s_watsonPyramid
 %
-% Formulae from the Watson Pyramid paper
+% Notes on the Watson Pyramid of Visibility (PoV) paper
 %
-%  
 %﻿The Field of View, the Field of Resolution, and the Field of Contrast Sensitivity  
 %
 
-% FOV (he calls Fixated FOV for FFoV)
-
+% Numbers to rremember
+%
 % When fixated, Beau claims FOV is -65 deg (in) to 105 deg (out)
 %                                  -74 deg (down) to 62 deg (up)
 %
+% Definitions: The highest spatial freq that can be detected with a
+% contrast = 1 was used to define the 'Window' of visibility.  This extends
+% to analysis say if the contrast is lower than one what range of spatial
+% and temporal frequencies can be detected.  Adding these lower contrasts
+% is how the pyramid (3D) is built on the window (2D).
+
+%% The Kelly spatial-temporal frequency contrast sensitivity function
+
+% In 1979 Don Kelly published a formula defining the space-time contrast
+% sensitivity function.  Suppose we define spatial frequency (cycles per
+% degree) and temporal frequency (hz).  ISETBio has an interface to return
+% the Kelly estimate
 
 
-%% Highest spatial freq that can be detected with contrast = 1
+% Watson says use linear spacing for his pyramid.  So we create a set of
+% linear sample values
+fs = linspace(1, 30, 80);
+ft = linspace(1, 60, 100);
+
+% Row is space, col is time
+contrastSensitivity = humanSpaceTime('kelly79', fs, ft);
+
+%  Visualize the Kelly data using a log(contrastSensitivity).  As Beau
+%  points out, the 
+surf(ft, fs, contrastSensitivity);
+set(gca, 'xscale', 'linear', 'yscale', 'linear','zscale','log');
+set(gca, 'xlim', [0 35], 'ylim', [0 20]);
+xlabel('Temporal freq (Hz)');
+ylabel('Spatial freq (cpd)')
+zlabel('Log CS');
+
+% I suggest rotating the surface in the window. 
+
+% From this point of view, the planar shape, and the low frequency drop, is
+% pretty clear.
+az = 45; el = 0;
+view([az,el]);
+
+%%  Viewed from the top down, you see the Window of visibility
+
+% Spatial and temporal frequencies inside the colored region are visible.
+% The white points would require a stimulus you can't really make, with a
+% contrast greater than 1.
+az = 0; el = 90;
+view([az el]);
+
+%%  How the Pyramid is built
+
+% For reasons I don't fully understand, Beau wants us to use negative
+% spatial and temporal frequencies.  What does a F and -F sin(2*piF*x) look
+% like? 
+F = 2;
+x = (0:0.01:1);
+vP = sin(2*pi*F*x);
+vN = sin(2*pi*(-1*F)*x);
+
+% These stimuli are basically the same, but with opposite contrast - black
+% is white and white is black. 
+ieNewGraphWin;
+p = plot(x,vP,'k-',x,vN,'k:');
+for ii=1:2, p(ii).LineWidth = 2; end
+legend({'Positive F','NegativeF '})
+
+%% So, let's make the pyramid by following Beau's recipe.
+
+Time = [fliplr(-ft),ft];   % The extended temporal frequencies
+Space = [fliplr(-fs),fs];  % The extended spatial frequencies
+
+% Nothing changes when you measure with positive or negative frequencies,
+% so we can just rotate then paste the same contrast sensitivity function
+% measured with all positive F and W into the other three quadrants, if F
+% and W are allowed to become negative.
+CS = [rot90(contrastSensitivity,2), flipud(contrastSensitivity);
+    flipud(rot90(contrastSensitivity,-2)), contrastSensitivity];
+
+%% Here is the Pyramid of Visibility.  
+
+% I think we will be calling it the Volcano of Visibility (VoV), though.
+[X,Y] = meshgrid(Time,Space);
+surf(X,Y,CS)
+ylabel('Space (cpd)');
+xlabel('Time (Hz)');
+zlabel('log CS')
+set(gca, 'xscale', 'linear', 'yscale', 'linear','zscale','log');
+
+%% Here is the 'Window of Visibility'
+
+% In this case, we are 
+ieNewGraphWin;
+imagesc(Time,Space,CS)
+ylabel('Space (cpd)');
+xlabel('Time (Hz)');
+colorbar;
+
+%% Eccentricity
+
+% Suppose we are at an eccentricity that scales the effective spatial
+% frequency by a factor of 2.  Then
+
+ieNewGraphWin;
+eccFactor = 2;
+surf(X,Y/eccFactor,CS)
+ylabel('Space (cpd)'); xlabel('Time (Hz)'); zlabel('log CS')
+set(gca, 'xscale', 'linear', 'yscale', 'linear','zscale','log');
+set(gca, 'xlim', [-40 40], 'ylim', [-40 40]);
+title(sprintf('Eccentricity factor %.1f',eccFactor));
+
+%% END
+
+%{
+For applications, we would like to know how the original shape measured by
+Kelly (and others varies with (a) eccentricity, (b) luminance level, and
+(c) color.  We would also like an image computable calculation that shows
+how to use this information.
+
+Beau describes his approach to including eccentricity in Equations (4) and
+(5). Scale the spatial frequency as if it is a higher spatial frequency
+than its true physical size, and the scaling factor is given by a linear
+dependence on eccentricity.
+
+Want to build a Matlab function that takes an image in and produces a
+sensitivity curve like the Volcano of Visibility for the fovea?  And then
+for different eccentricities.
+%}
+
+
+%% Paper notes 
 %
-% (We should conditionalize on size and duration of the grating, also mean
-% luminance/color) 
-%
-% What is the formula for the CSF in Figure 1?  Maybe the formula is in
-% Refs 5-9?  Not obvious, though.
-
-
-%% The pyramid scheme
+% The first linear equation
 %
 %    (Equation 1)
 %    S = C0 + CW W + CF F + CL L
@@ -38,7 +154,11 @@
 %
 %    S = C0' + CW W + CF F
 % 
-% Which is Figure 2.  TO be a pyramid, notice, we need both positive and
+% This is probably OK for the positive quadrant.  If he had put an absolute
+% value around W and F then values in the other three quadrants would
+% always be the same as the value in the all positive quadrant.
+%
+% Which is Figure 2.  To be a pyramid, notice, we need both positive and
 % negative frequency values.  Hmmm.  What does that mean exactly?  It
 % imposes symmetry in some sense.  But the formula for the pyramid doesn't
 % really work with that.  If we have +F  and -F the S valuke would be quite
@@ -58,73 +178,12 @@
 %        F =  -W + K
 %        W =  F - K
 
-%% What's the pyramid?
+%% Questions and Notes
 %
-% I know what we mean when we plot positive spatial and temporal frequency.
-%  Don Kelly provided a summary of 15 years of work in a paper in 1979
-%  showing S = log(contrast sensitivity) as a function of these two
-%  variable.s
+% (We should conditionalize on size and duration of the grating, also mean
+% luminance/color) 
+%
+% What is the formula for the CSF in Figure 1?  Maybe the formula is in
+% Refs 5-9?  Not obvious, though.
 
-% fs = logspace(-0.2, log10(30), 80);
-% ft = logspace(-0.2, log10(60), 100);
-
-%%
-
-% The Kelly spatial-temporal frequency contrast sensitivity function
-% Row is space, col is time
-fs = linspace(1, 30, 80);
-ft = linspace(1, 60, 100);
-sens = humanSpaceTime('kelly79', fs, ft);
-
-% Here is it
-surf(ft, fs, sens);
-set(gca, 'xscale', 'linear', 'yscale', 'linear');
-set(gca, 'xlim', [0 35], 'ylim', [0 20]);
-xlabel('Temporal freq (Hz)');
-ylabel('Spatial freq (cpd)')
-
-%% Where's the pyramid?
-
-% Rotate Kelly's surface around so you can see the planar side.
-az = 57.8821; el = 7.0814;
-view([az,el]);
-
-%%
-% This is one of the sides of Beau's Pyramid.  Points outside of the
-% surface are invisible, and points inside of the surface are visible.
-% 
-az = 0; el = 90;
-view([az el]);
-
-%%  Suppose we want to plot -F and -W, too.
-
-% What does a F and -F sin(2*piF*x) look like?
-F = 2;
-x = (0:0.01:1);
-vP = sin(2*pi*F*x);
-vN = sin(2*pi*(-1*F)*x);
-
-% They are just opposite contrast - black is white and white is black.
-ieNewGraphWin;
-p = plot(x,vP,'k-',x,vN,'k:');
-for ii=1:2, p(ii).LineWidth = 2; end
-legend({'Positive F','NegativeF '})
-
-%% The pyramid
-
-CS = [rot90(sens,2), flipud(sens);
-    flipud(rot90(sens,-2)), sens];
-
-Time = [fliplr(-ft),ft];
-Space = [fliplr(-fs),fs];
-imagesc(Time,Space,CS)
-
-%%
-[X,Y] = meshgrid(Time,Space);
-surf(X,Y,CS)
-ylabel('space');
-xlabel('time');
-zlabel('CS')
-
-%% END
 
